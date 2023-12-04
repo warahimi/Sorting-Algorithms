2fbe4fdc57805076261b4fa55de1d9a6cd866076
Wahidullah Rahimi <wahidrahimi45@gmail>
2023-12-04 14:04:18 -0800

    bubble sort

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortingAlgorithms s = new SortingAlgorithms();
+            int[] arr = { 20,30,5,6,7,8 };
+            s.bubbleSort(arr);
+            s.print(arr);
+
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class SortingAlgorithms
+    {
+        public void bubbleSort(int[] arr)
+        {
+            for(int i = 0; i < arr.Length - 1; i++)
+            {
+                bool swapped = false;
+                for(int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                        swapped = true;
+                    }
+                }
+                if(!swapped)
+                {
+                    break;
+                }
+            }
+        }
+
+        public void print(int[] arr) { 
+            foreach(var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+    }
+}
